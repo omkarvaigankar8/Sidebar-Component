eac2fcecdffb51cf1339615b070ae2c4f4c8a008
Omkar Vaigankar <omkarvaigankar8@gmail.com>
2021-04-20 11:57:32 +0530

    Backdrop added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,9 +5,9 @@ import * as AiIcons from "react-icons/ai";
 import {SidebarData} from './SidebarData';
 import './Sidebar.css';
 import {IconContext} from 'react-icons';
+import Backdrop from './Backdrop';
 
-
-function Sidebar() {
+function Sidebar(props) {
     const [sidebar,setSidebar] =useState(false);
 
     const showSidebar=() => setSidebar(!sidebar);
@@ -20,7 +20,8 @@ function Sidebar() {
                     <FaIcons.FaBars  onClick={showSidebar}/>
                 </Link>
             </div>
-            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+            {sidebar ? <Backdrop /> : null}
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'} >
                 <ul className="nav-menu-items" onClick={showSidebar}>
                     <li className="navbar-toggle">
                         <Link to="#" className='menu-bars'>
